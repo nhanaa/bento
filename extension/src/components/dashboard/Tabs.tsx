41f92c6afd56e67f587240d7c5b8b6ae7b390640
mellieho9 <melioraho9@gmail.com>
2024-06-02 21:39:55 -0400

    edited the UI

--- extension/src/components/dashboard/Tabs.tsx
+++ extension/src/components/dashboard/Tabs.tsx
@@ -1,34 +1,25 @@
-import { Folders, UserRound } from 'lucide-react'
-import React from 'react'
+import { Folders, UserRound } from "lucide-react";
+import React from "react";
+import { Tab } from "./Tab";
 
 interface TabsProps {
-  tab: string
-  setTab: (tab: 'folders' | 'settings') => void
+  tab: string;
+  setTab: (tab: "folders" | "settings") => void;
 }
 
 export const Tabs = ({ tab, setTab }: TabsProps) => {
   return (
-    <div className="w-full flex space-x-1 mt-2">
-      <button
-        onClick={() => setTab('folders')}
-        className={`w-1/2 flex items-center justify-center py-2 px-4 rounded-none transition-colors border-0 ${
-          tab === 'folders'
-            ? 'border-t-2 border-customViolet'
-            : 'border-t-2 border-transparent'
-        }`}
-      >
-        <Folders className="w-6 h-6 text-customViolet" />
-      </button>
-      <button
-        onClick={() => setTab('settings')}
-        className={`w-1/2 flex items-center justify-center py-2 px-4 rounded-none transition-colors border-0 ${
-          tab === 'settings'
-            ? 'border-t-2 border-customViolet'
-            : 'border-t-2 border-transparent'
-        }`}
-      >
-        <UserRound className="w-6 h-6 text-customViolet" />
-      </button>
+    <div className="relative bottom-0 w-full flex space-x-1 mt-2">
+      <Tab
+        isActive={tab === "folders"}
+        onClick={() => setTab("folders")}
+        icon={<Folders className="w-6 h-6 text-customViolet" />}
+      />
+      <Tab
+        isActive={tab === "settings"}
+        onClick={() => setTab("settings")}
+        icon={<UserRound className="w-6 h-6 text-customViolet" />}
+      />
     </div>
-  )
-}
+  );
+};
